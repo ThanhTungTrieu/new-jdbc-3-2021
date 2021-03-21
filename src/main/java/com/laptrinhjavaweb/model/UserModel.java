06428b40d8e04b61a9e1f4d015eec93f54037cf1
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-03-21 18:21:09 +0700

    Model update

--- src/main/java/com/laptrinhjavaweb/model/UserModel.java
+++ src/main/java/com/laptrinhjavaweb/model/UserModel.java
@@ -1,24 +1,51 @@
 package com.laptrinhjavaweb.model;
 
-public class UserModel {
+public class UserModel extends AbstractModel {
 
-	private String userName;
-	private String fullName;
+	private String username;
+	private String fullname;
+	private String password;
+	private int status;
+	private Long roleId;
 
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public String getFullName() {
-		return fullName;
+	public String getFullname() {
+		return fullname;
 	}
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
 	}
 
 }
